6dbcb6dae4533ad9589e00d9839225fbfdb9a103
Jared <kuil5230@gmail.com>
2018-05-18 21:33:03 +0800

    .

--- server/chinese.js
+++
@@ -1,8 +0,0 @@
-const { tify, sify } = require("chinese-conv");
-const fs = require("fs");
-
-fs.readFile("./static/dict.txt", "utf-8", (err, data) => {
-  let text = tify(data);
-
-  fs.writeFile("./static/chinese.txt", text);
-});
